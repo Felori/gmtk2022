73e0a7a191a927e58a805ba2a77057b7b5adba88
Antti Kangas <antti.kangas99@gmail.com>
2022-07-15 21:53:17 +0300

    Add basic grid generation

---
+++ Assets/Scripts/GameMap.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMap : MonoBehaviour
+{
+    [SerializeField] GameTile tilePrefab = default;
+
+    GameTile[,] tilemap;
+
+    void GenerateMap(int width, int height)
+    {
+        tilemap = new GameTile[width, height];
+
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                CreateTile(x, y);
+            }
+        }
+    }
+
+    void CreateTile(int x, int y)
+    {
+        GameTile tile = Instantiate(tilePrefab, transform);
+        tile.transform.localPosition = new Vector3(x, 0, y);
+        tilemap[x, y] = tile;
+    }
+
+    private void Start()
+    {
+        GenerateMap(10, 10);
+    }
+}
